204199038a858a5eba0713ede71628da4dee5eeb
Fabrizio Maffoni <125399775+elFabri05@users.noreply.github.com>
2024-05-08 19:08:28 +0200

    env variables + .toml file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+    // Common variables across environments
+    readonly VITE_API_URL: string;
+  
+    // Backend-specific variables
+    readonly MONGO_URI: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
