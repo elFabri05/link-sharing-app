cb822ba7ffccd37296ddca44c349cfb981b0f092
Fabrizio Maffoni <125399775+elFabri05@users.noreply.github.com>
2024-05-12 20:09:54 +0200

    tsconfigPaths installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 
 export default ({ mode }) => {
     const env = loadEnv(mode, process.cwd(), 'VITE_');
 
     return defineConfig({
-        plugins: [react()],
+        plugins: [react(), tsconfigPaths()],
         server: {
             port: parseInt(env.VITE_PORT) || 3302, 
             strictPort: true, 
