e58a0eff3ad18b4df480062d2c4bc45ed336cda4
Fabrizio Maffoni <125399775+elFabri05@users.noreply.github.com>
2024-11-06 19:56:58 -0300

    config + env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
@@ -7,11 +7,8 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
-      port: parseInt(env.VITE_PORT, 10) || 3000,  // Use VITE_PORT from .env, fallback to 3000
-      strictPort: true,  // If the port is already in use, it will not increment to another
+      port: parseInt(env.VITE_PORT, 10) || 3000,
+      strictPort: true,
     },
-    define: {
-      'process.env.ALLOWED_ORIGIN': JSON.stringify(process.env.ALLOWED_ORIGIN || 'http://localhost:3000')
-    }
   };
 });
