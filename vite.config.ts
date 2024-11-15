09cd689b8f89a45590d40c298f011ba9c0f70f29
Fabrizio Maffoni <125399775+elFabri05@users.noreply.github.com>
2024-11-15 19:06:49 -0300

    Reconfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/link-sharing-app/',
   plugins: [react()],
   server: {
     port: 3000,
